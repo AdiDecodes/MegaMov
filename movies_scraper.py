39c1be227dac1d2c3b36c4962f7dcb469ed00503
Aditya Singh <aditya@cleverstudio.in>
2023-01-29 23:57:41 +0530

    Updated some servers

--- movies_scraper.py
+++ movies_scraper.py
@@ -10,13 +10,14 @@ def search_movies(query):
     movies_list = []
     movies_details = {}
     website = BeautifulSoup(requests.get(
-        f"https://185.53.88.104/?s={query.replace(' ', '+')}").text, "html.parser")
-    movies = website.find_all("a", {'class': 'ml-mask jt'})
+        f"https://hdbollyhub.shop/?s={query.replace(' ', '+')}").text, "html.parser")
+    movies = website.find_all(
+        "a", {'class': 'simple-grid-grid-post-thumbnail-link'})
     for movie in movies:
         if movie:
             movies_details["id"] = f"link{movies.index(movie)}"
             movies_details["title"] = movie.find(
-                "span", {'class': 'mli-info'}).text
+                "img", {'class': 'simple-grid-grid-post-thumbnail-img wp-post-image'}).get("title").replace("Download", "").strip()
             url_list[movies_details["id"]] = movie['href']
         movies_list.append(movies_details)
         movies_details = {}
@@ -28,12 +29,15 @@ def get_movie(query):
     movie_page_link = BeautifulSoup(requests.get(
         f"{url_list[query]}").text, 'html.parser')
     if movie_page_link:
-        title = movie_page_link.find("div", {'class': 'mvic-desc'}).h3.text
-        movie_details["title"] = title.replace('mkvCinemas.mkv', ">>")
-        img = movie_page_link.find("div", {'class': 'mvic-thumb'})['data-bg']
+        title = movie_page_link.find(
+            "img", {'class': 'simple-grid-post-thumbnail-single-img wp-post-image'}).get('title')
+        print(title)
+        movie_details["title"] = title.replace("Download", "").strip()
+        img = movie_page_link.find(
+            "img", {'class': 'simple-grid-post-thumbnail-single-img wp-post-image'}).get('src')
         movie_details["img"] = img
         links = movie_page_link.find_all(
-            "a", {'rel': 'noopener', 'data-wpel-link': 'internal'})
+            "a", {'class': 'maxbutton-2 maxbutton maxbutton-12'})
         final_links = {}
         for i in links:
             url = f"https://urlshortx.com/api?api={API}&url={i['href']}"
@@ -45,3 +49,7 @@ def get_movie(query):
             new_key = key.replace("mkvCinemas.mkv", "MegaMov")
             final_links[new_key] = final_links.pop(key)
     return movie_details
+
+
+print(search_movies('Taaza Khabar'))
+print(get_movie('link0'))
